0d81fc3b48e63a56c19f71c6612f6d8eb1ddc8f8
Ye Thiha <mightycryptowall@gmail.com>
2021-10-02 23:51:00 +0630

    useDimension -t

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {
+  Dimensions,
   StyleSheet,
   SafeAreaView,
   Button,
   Alert,
+  View,
 } from "react-native";
 import SafeViewAndroid from "./components/SafeViewAndroid";
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
+ let dimension = useDimensions();
+ let orientation = useDeviceOrientation();
   return (
-    <SafeAreaView style={[styles.container, SafeViewAndroid.SafeAreaView]}>
-      <Button 
-        color= "orange"
-        title="Click Me" 
-        onPress={() => 
-          Alert.prompt("My title", "My message", text => console.log(text))} 
-      />
+    <SafeAreaView style={[styles.container, SafeViewAndroid.AndroidSafeArea]}>
+      <View style={{
+        backgroundColor: "dodgerblue",
+        width: "100%",
+        height: orientation.landscape ? "100%":"30%"
+      }}>
+        <Button title="hello" onPress={() =>  console.log(dimension.screen,orientation)}/>
+      </View>
     </SafeAreaView>
   );
 }
@@ -27,7 +33,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center"
   },
 });
