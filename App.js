46c1a26d4727c8524db78bddb5bc6854550a5e0b
Ye Thiha <mightycryptowall@gmail.com>
2021-10-05 10:01:36 +0630

    welcome & view image screens -a

--- App.js
+++ App.js
@@ -1,45 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 import SafeViewAndroid from "./components/SafeViewAndroid";
 
 export default function App() {
   return (
     <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea, styles.container]}>
-      <View
-        style={{
-          backgroundColor: "#fff",
-          flex: 1,
-          flexDirection: "row", // horizontal
-          justifyContent: "center", // main
-          alignItems: "center", // secondary
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "dodgerblue",
-            width: 100,
-            height: 100,
-          }}
-        />
-        <View
-          style={{
-            backgroundColor: "gold",
-            width: 100,
-            height: 100,
-            top: 20,
-            left: 20,
-            position: "absolute"
-          }}
-       />
-        <View
-          style={{
-            backgroundColor: "tomato",
-            width: 100,
-            height: 100,
-          }}
-        />
-      </View>
+      {/* <WelcomeScreen/> */}
+      <ViewImageScreen/>
     </SafeAreaView>
   );
 }
