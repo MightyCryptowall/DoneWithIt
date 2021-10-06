27322e622f2c7d70c602284d7edb0584a2f4094b
Ye Thiha <mightycryptowall@gmail.com>
2021-10-06 22:31:51 +0630

    encapsulating styles

--- App.js
+++ App.js
@@ -3,24 +3,15 @@ import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-import SafeViewAndroid from "./components/SafeViewAndroid";
+import SafeViewAndroid from "./app/components/SafeViewAndroid";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
     <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea, styles.container]}>
-      <Text
-       style={{
-        //  fontFamily: "Roboto", //Courier for ios and Roboto for androidd
-         fontSize: 30,
-         fontStyle: "italic",
-         fontWeight: "600",
-         color: "tomato",
-         textTransform: "capitalize",
-        //  textDecorationLine: "line-through",
-        textAlign: "center",
-        lineHeight: 35
-       }}
-      >I love React Native! This is my first react native app! Here's some more text.</Text>
+      <AppText>
+        I love React Native!
+      </AppText>
     </SafeAreaView>
   );
 }
