01afa8c47e11b36cb14a214c72768492fd3291e9
Ye Thiha <mightycryptowall@gmail.com>
2021-10-10 21:41:29 +0630

    welcome screen -u

--- App.js
+++ App.js
@@ -4,29 +4,21 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import SafeViewAndroid from "./app/components/SafeViewAndroid";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  console.log("hello");
   return (
-
-   
     <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea, styles.container]}>
-       <StatusBar
-     backgroundColor="black"
-     style="light"
-   />
-      <WelcomeScreen/>
+      <StatusBar backgroundColor="black" style="light" />
+      <WelcomeScreen />
     </SafeAreaView>
-
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
 });
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -5,14 +5,17 @@ import AppButton from '../components/AppButton';
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
+        blurRadius={3}
         style={styles.background}
         source={require("../assets/background.jpg")}>
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require("../assets/logo-red.png") }/>
-                <Text>Sell What You Don't Need</Text>
+                <Text style={styles.tagline}>Sell What You Don't Need</Text>
             </View>
-            <AppButton title="Login" onPress={() => console.log("Tapped")} />
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonContainer}>
+                <AppButton title="Login" onPress={() => console.log("Tapped Login Button")} />
+                <AppButton title="Register" color="secondary" onPress={() => console.log("Tapped Register Button")} />
+            </View>
         </ImageBackground>
     );
 }
@@ -24,6 +27,10 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "center"
     },
+    buttonContainer: {
+        padding:20,
+        width: "100%"
+    },
     logo: {
         width: 100,
         height: 100,
@@ -31,13 +38,13 @@ const styles = StyleSheet.create({
     },
     logoContainer: {
         position: "absolute",
-        top: 70,
+        top: 100,
         alignItems: "center"
     },
-    registerButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#4ecdc4"
+    tagline: {
+        fontSize: 25,
+        fontWeight: "600",
+        paddingVertical: 20
     },
 })
 
