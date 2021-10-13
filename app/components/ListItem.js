c430a1a8dafad607037ae2995f24c21809b71856
Ye Thiha <mightycryptowall@gmail.com>
2021-10-13 12:58:13 +0630

    extended listItem -c

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -5,15 +5,16 @@ import AppText from "./AppText";
 import colors from "../config/colors";
 import { Swipeable } from "react-native-gesture-handler";
 
-function ListItem({ title, subtitle, image, onPress, renderRightActions }) {
+function ListItem({ title, subtitle, image, ImageComponent, onPress, renderRightActions }) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
         <View style={styles.container}>
-          <Image style={styles.image} source={image} />
-          <View>
+          {ImageComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subtitle}>{subtitle}</AppText>
+            {subtitle && <AppText style={styles.subtitle}>{subtitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -26,11 +27,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     padding: 8,
   },
+  detailContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
   },
   subtitle: {
     color: colors.medium,
