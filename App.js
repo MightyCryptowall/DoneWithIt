a75eb1b3c5570fff5970b7b48221dfc53152541e
Ye Thiha <mightycryptowall@gmail.com>
2021-10-13 12:48:02 +0630

    icon -c -a

--- App.js
+++ App.js
@@ -9,11 +9,21 @@ import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
   return (
+    <Screen>
+      <Icon
+        name="email"
+        size={100}
+        backgroundColor="red"
+        iconColor="white"
+      />
 
-    <MessagesScreen />
+    </Screen>
+    // <MessagesScreen />
     //   {/* <WelcomeScreen /> */}
     //  {/* <ListingDetailsScreen/> */}
     //  {/* <ViewImageScreen /> */}
