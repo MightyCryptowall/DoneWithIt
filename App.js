6a1e6ff6eb4c72cd601857e95ef2209b56c033bb
Ye Thiha <mightycryptowall@gmail.com>
2021-10-16 20:04:24 +0630

    accessing the image library -f -a

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import { Button, Image, StyleSheet } from "react-native";
 import Screen from "./app/components/Screen";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 
 export default function App() {
+  
+  const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
-    // const { granted } = await Permissions.askAsync(
-    //   Permissions.MEDIA_LIBRARY,
-    //   Permissions.LOCATION_BACKGROUND
-    // );
     const { granted } = await ImagePicker.requestCameraPermissionsAsync();
     if (!granted) alert("You need to enable permission to access the library.");
   };
@@ -18,7 +17,20 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if(!result.cancelled)
+        setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an image", error)
+    }
+  }
+
+  return <Screen>
+    <Button title="Select Image" onPress={selectImage} />
+    <Image source={{ uri: imageUri }} style={{width: 200, height: 200}} />
+  </Screen>;
 }
 
 const styles = StyleSheet.create({
