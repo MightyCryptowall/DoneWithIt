76b326ff3573f6d8f79f17fb2b57a03354485bb1
Ye Thiha <mightycryptowall@gmail.com>
2021-11-02 22:19:38 +0630

    native-stack -t

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import React, { useEffect, useState } from "react";
-import { Button, Image, StyleSheet } from "react-native";
+import React from "react";
+import { Button, StyleSheet, Text } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  NavigationContainer,
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
+
 import Screen from "./app/components/Screen";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
-import ImageInputList from "./app/components/ImageInputList";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+const Link = () => {
+  const navigation = useNavigation();
+
+  return (
+    <Button
+      title="Click"
+      onPress={() =>
+        navigation.navigate("TweetDetails", {
+          id: 1,
+        })
+      }
+    />
+  );
+};
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Link />
+  </Screen>
+);
+
+const TweetDetails = () => {
+  const route = useRoute();
+
+  return (
+    <Screen>
+      <Text>Tweet Details {route.params.id}</Text>
+    </Screen>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerTitleAlign: "center",
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}
+  >
+    <Stack.Screen
+      name="Tweets"
+      component={Tweets}
+      options={{
+        headerTitleAlign: "center",
+        headerStyle: { backgroundColor: "tomato" },
+        headerTintColor: "white",
+        headerShown: false,
+      }}
+    />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={({ route }) => ({
+        title: route.params.id.toString(),
+      })}
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // padding: 20,
-    // paddingTop: 50,
-    backgroundColor: "#111",
-  },
+  container: {},
 });
