b1b67d024513a7ad387f006b136dbb537e82db6a
Ye Thiha <mightycryptowall@gmail.com>
2021-11-23 12:06:14 +0630

    authContext -a

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, StyleSheet, Text } from "react-native";
 import {
   createNativeFeedNavigator,
@@ -19,18 +19,22 @@ import AppNavigator from "./app/navigation/AppNavigator";
 import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import OfflineNotice from "./app/components/OfflineNotice";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import AuthContext from "./app/auth/context";
 
 export default function App() {
   // const netInfo = useNetInfo();
 
+
+  const [user, setUser] = useState();
+
   return (
-    <>
-    
+    <AuthContext.Provider value={{user, setUser}}>
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        {user ? <AppNavigator/> : <AuthNavigator />}
       </NavigationContainer>
-      <OfflineNotice />
-    </>
+      {/* <OfflineNotice /> */}
+    </AuthContext.Provider>
   );
 }
 
