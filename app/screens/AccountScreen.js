f30602c2312ed45cb3c3a6a72156ae807ed395f8
Ye Thiha <mightycryptowall@gmail.com>
2021-11-23 12:09:05 +0630

    accountScreen -u

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,78 +1,80 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
-import Icon from '../components/Icon';
+import { useNavigation } from "@react-navigation/native";
+import React, { useContext } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import AuthContext from "../auth/context";
+import Icon from "../components/Icon";
 
-import ListItem from '../components/ListItem';
-import ListItemSeparator from '../components/ListItemSeparator';
-import Screen from '../components/Screen';
-import colors from '../config/colors';
-import routes from '../navigation/routes';
+import ListItem from "../components/ListItem";
+import ListItemSeparator from "../components/ListItemSeparator";
+import Screen from "../components/Screen";
+import colors from "../config/colors";
+import routes from "../navigation/routes";
 
 const menuItems = [
-    {
-        title: "My Listings",
-        icon: {
-            name: "format-list-bulleted",
-            backgroundColor: colors.primary,
-        }
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
     },
-    {
-        title: "My Messages",
-        icon: {
-            name: "email",
-            backgroundColor: colors.secondary,
-        },
-        targetScreen: routes.MESSAGES
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
     },
-]
+    targetScreen: routes.MESSAGES,
+  },
+];
 
 function AccountScreen(props) {
+  const navigation = useNavigation();
+  const { user } = useContext(AuthContext);
 
-    const navigation = useNavigation();
-
-    return (
-        <Screen style={styles.screen}>
-            <View style={styles.container}>
-            <ListItem 
-                title="Ye Thiha"
-                subtitle="developer@yethiha.com"
-                image={require("../assets/mosh.jpg")}
-            />
-            </View>
-            <View style={styles.container}>
-                <FlatList 
-                    data={menuItems}
-                    keyExtractor={menuItem => menuItem.title}
-                    ItemSeparatorComponent = {ListItemSeparator}
-                    renderItem={ ({item}) => 
-                    <ListItem
-                        title={item.title}
-                        IconComponent={
-                            <Icon name={item.icon.name} backgroundColor={item.icon.backgroundColor} />
-                        }
-                        onPress= {() => navigation.navigate(item.targetScreen)}
-                    />
-                    }
+  return (
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <ListItem
+          title={user.name}
+          subtitle={user.email}
+          image={require("../assets/mosh.jpg")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          ItemSeparatorComponent={ListItemSeparator}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
                 />
-            </View>
-            <ListItem 
-                title="Log Out"
-                IconComponent = {
-                    <Icon name="logout" backgroundColor="#ffe66d" />
-                }
+              }
+              onPress={() => navigation.navigate(item.targetScreen)}
             />
-        </Screen>
-    );
+          )}
+        />
+      </View>
+      <ListItem
+        title="Log Out"
+        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+      />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginVertical: 20
-    },
-    screen: {
-        backgroundColor: colors.light,
-    }
-})
+  container: {
+    marginVertical: 20,
+  },
+  screen: {
+    backgroundColor: colors.light,
+  },
+});
 
 export default AccountScreen;
